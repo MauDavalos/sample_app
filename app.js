717a1e661aadf7dfe2663fa46ffede5e5296dff9
MAURICIO ANDRÉS DÁVALOS JÁCOME <madvalosj@espe.edu.ec>
2021-05-24 23:29:36 -0500

    exclusivo dos

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("aqui indicaremos un nuevo tag")
 console.log("aqui indicaremos un nuevo tag")
 
 console.log("esto es contenido exlclusivo del ejemplo")
+console.log("contenido exclusivo 2")
