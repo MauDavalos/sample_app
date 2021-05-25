b2c3bd515942dd67ae8fb04abcf33fea77eee9bd
MAURICIO ANDRÉS DÁVALOS JÁCOME <madvalosj@espe.edu.ec>
2021-05-24 23:04:49 -0500

    nuevas modificaciones

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 console.log("Hello")
 console.log("aqui indicaremos un nuevo tag")
 console.log("aqui indicaremos un nuevo tag")
+
+
+console.log("hola estamos en el branch ejemplo")
+console.log("este es el contenido de ejemplo")
+
+
+
